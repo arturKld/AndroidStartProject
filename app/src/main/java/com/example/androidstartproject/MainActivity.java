9f3a538e5b0503f9f537498f5f0255ff460831c3
arturKld <article9899@gmail.com>
2023-04-16 15:57:12 +0200

    Clean up of project

--- app/src/main/java/com/example/androidstartproject/MainActivity.java
+++ app/src/main/java/com/example/androidstartproject/MainActivity.java
@@ -13,10 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-
-        TextView textView = (TextView) findViewById(R.id.normal);
-        textView.setText("Text from Java");
-        textView.setTextColor(Color.RED);
+        setContentView(R.layout.activity_two);
     }
 }
