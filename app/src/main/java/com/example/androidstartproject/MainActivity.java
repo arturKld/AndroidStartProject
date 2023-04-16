0b8bf1fa3b45ad7aec0b29aba94a762a8abdc981
arturKld <article9899@gmail.com>
2023-04-16 18:58:42 +0200

    first calculator done

--- app/src/main/java/com/example/androidstartproject/MainActivity.java
+++ app/src/main/java/com/example/androidstartproject/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_two);
+        setContentView(R.layout.activity_calculator);
     }
 }
